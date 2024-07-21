aec412a145e8d15d00e99bf4820bbbd3dc7a776f
Ubuntu <ubuntu@ip-172-31-17-236.us-east-2.compute.internal>
2024-07-21 03:13:10 +0000

    Added some tweaks

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, jsonify
 from flask_mysqldb import MySQL
 
 app = Flask(__name__)
@@ -12,6 +12,18 @@ app.config['MYSQL_DB'] = os.environ.get('MYSQL_DB', 'default_db')
 
 # Initialize MySQL
 mysql = MySQL(app)
+
+def init_db():
+    with app.app_context():
+        cur = mysql.connection.cursor()
+        cur.execute('''
+        CREATE TABLE IF NOT EXISTS messages (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            message TEXT
+        );
+        ''')
+        mysql.connection.commit()  
+        cur.close()
 
 @app.route('/')
 def hello():
@@ -28,8 +40,9 @@ def submit():
     cur.execute('INSERT INTO messages (message) VALUES (%s)', [new_message])
     mysql.connection.commit()
     cur.close()
-    return redirect(url_for('hello'))
+    return jsonify({'message': new_message})
 
 if __name__ == '__main__':
+    init_db()
     app.run(host='0.0.0.0', port=5000, debug=True)
 
